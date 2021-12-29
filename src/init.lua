4408a77ce9d4adabae85ae0fa3ec0a1524d1683f
MuXiu1997 <muxiu1997@gmail.com>
2021-12-29 11:47:56 +0800

    feat: 🎸 init.lua

---
+++ src/init.lua
@@ -0,0 +1,13 @@
+function _G.pprint(...)
+    local objects = {}
+    for i = 1, select('#', ...) do
+        local v = select(i, ...)
+        table.insert(objects, vim.inspect(v))
+    end
+
+    print(table.concat(objects, '\n'))
+    return ...
+end
+
+require('core')
+
