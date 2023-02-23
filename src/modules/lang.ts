f2e3c64001045e67d5ed11818a00a5cd2a0423ab
MuXiu1997 <muxiu1997@gmail.com>
2023-02-23 17:41:39 +0800

    feat: Update nvim-treesitter config

--- src/modules/lang.ts
+++ src/modules/lang.ts
@@ -4,8 +4,11 @@ const lang = defineModule({
   'nvim-treesitter/nvim-treesitter': {
     event: 'BufRead',
     config: () => {
+      typeRequire<{ prefer_git: boolean }>(
+        'nvim-treesitter.install'
+      ).prefer_git = true
       typeRequire<Setupable>('nvim-treesitter.configs').setup({
-        ensure_installed: 'maintained',
+        ensure_installed: 'all',
         highlight: {
           enable: true,
         },
