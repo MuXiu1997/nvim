ac41a0d9e80c9a9f3501dcba1471809ba25fc89c
MuXiu1997 <muxiu1997@gmail.com>
2024-03-19 17:43:43 +0800

    chore: add `eslint` config

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['@muxiu1997'],
-  rules: {
-    '@typescript-eslint/no-var-requires': 'off',
-  },
-}
---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+// noinspection JSUnusedGlobalSymbols
+
+import antfu from '@antfu/eslint-config'
+
+/** @type {import('@antfu/eslint-config').UserConfigItem[]} */
+const overrides = [
+  {
+    files: ['**/*.js', '**/*.ts', '**/*.vue'],
+    rules: {
+      'import/order': [
+        'error',
+        {
+          'groups': ['index', 'sibling', 'parent', 'internal', 'external', 'builtin', 'object', 'unknown', 'type'],
+          'newlines-between': 'always',
+          'alphabetize': {
+            order: 'asc',
+          },
+        },
+      ],
+      'curly': ['error', 'multi-line', 'consistent'],
+      'antfu/if-newline': ['off'],
+      'ts/no-require-imports': 'off',
+      'ts/no-var-requires': 'off',
+      'style/member-delimiter-style': 'off',
+    },
+  },
+  {
+    files: ['**/*.vue'],
+    rules: {
+      'vue/max-attributes-per-line': [
+        'error',
+        {
+          singleline: 1,
+          multiline: 1,
+        },
+      ],
+    },
+  },
+]
+
+export default await antfu({}, ...overrides)
